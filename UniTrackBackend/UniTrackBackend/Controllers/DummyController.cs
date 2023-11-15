41524bf60c532dd5a2eff9eb6dd6b7c72daa74d0
Spas Milenkov <spasmilenkov123@gmail.com>
2023-11-15 12:01:05 +0200

    Workflow test

---
+++ UniTrackBackend/UniTrackBackend/Controllers/DummyController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UniTrackBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DummyController : ControllerBase
+    {
+    }
+}
