39db33832d6dcb8440ff9d63e28413ee3a786c4f
Spas Milenkov <spasmilenkov123@gmail.com>
2023-11-16 16:45:35 +0200

    Set up JWT token handling and swagger gen, add authentication and cors policy for localhost testing

--- UniTrackBackend/UniTrackBackend/Program.cs
+++ UniTrackBackend/UniTrackBackend/Program.cs
@@ -1,10 +1,71 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using UniTrackBackend.Infrastructure;
+using UniTrackBackend.Interfaces;
+using UniTrackBackend.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("AllowOrigin",
+        options => options.WithOrigins(
+                "https://localhost:5500", "http://127.0.0.1:5500")
+            .AllowCredentials()
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+    );
+});
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration.GetSection("Jwt:Key").Value!)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ClockSkew = TimeSpan.Zero
+        };
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                if (context.Request.Cookies.ContainsKey("AccessToken"))
+                {
+                    context.Token = context.Request.Cookies["AccessToken"];
+                }
 
+                return Task.CompletedTask;
+            }
+        };
+    });
+builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard authorization header using bearer token",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+
+});
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -16,6 +77,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowOrigin");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
