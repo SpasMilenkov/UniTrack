fa14f87911d0d75e5514a0aaf0d4cbc453ff9f80
Spas Milenkov <spasmilenkov123@gmail.com>
2023-11-16 16:42:46 +0200

    Add registration view model

---
+++ UniTrackBackend/UniTrackBackend/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace UniTrackBackend.ViewModels;
+
+public class RegisterViewModel
+{
+    [Required(ErrorMessage = "Username is required.")]
+    [StringLength(50, ErrorMessage = "Username cannot be longer than 50 characters.")]
+    public required string UserName { get; set; }
+    
+    [Required(ErrorMessage = "First name is required.")]
+    [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+    public required string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Last name is required.")]
+    [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
+    public required string LastName { get; set; }
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
+    [StringLength(100, ErrorMessage = "Email cannot be longer than 100 characters.")]
+    public required string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 100 characters long.")]
+    public required string Password { get; set; }
+
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public required string ConfirmPassword { get; set; }
+    
+}
