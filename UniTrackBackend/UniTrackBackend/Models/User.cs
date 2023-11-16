2e974dacc56bf02fd121ca4c8e80226dbb56b834
Spas Milenkov <spasmilenkov123@gmail.com>
2023-11-16 16:40:31 +0200

    Add the main user model

---
+++ UniTrackBackend/UniTrackBackend/Models/User.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace UniTrackBackend.Models;
+
+public class User : IdentityUser
+{
+
+    public required string FirstName { get; set; }
+
+    public required string LastName { get; set; }
+
+    public string? RefreshToken { get; set; }
+    
+    public DateTime? RefreshTokenValidity { get; set; }
+}
