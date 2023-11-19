7216f1360a474aa8c77e289325ebfa0b2cc0a51f
Spas Milenkov <spasmilenkov123@gmail.com>
2023-11-19 18:49:39 +0200

    Move the db context and add the new entities as tables

---
+++ UniTrackBackend/UniTrackBackend.Data/Database/UniTrackDbContext.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using UniTrackBackend.Data.Models;
+
+namespace UniTrackBackend.Data.Database;
+
+public class UniTrackDbContext : IdentityDbContext<User>
+{
+    public UniTrackDbContext(DbContextOptions<UniTrackDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Absence> Absences { get; set; } = null!;
+    public DbSet<ElectiveSubject> ElectiveSubjects { get; set; } = null!;
+    public DbSet<Grade> Grades { get; set; } = null!;
+    public DbSet<Mark> Marks { get; set; } = null!;
+    public DbSet<Parent> Parents { get; set; } = null!;
+    public DbSet<Student> Students { get; set; } = null!;
+    public DbSet<Subject> Subjects { get; set; } = null!;
+    public DbSet<Teacher> Teachers { get; set; } = null!;
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Parent>()
+            .HasOne(p => p.User)
+            .WithOne()
+            .HasForeignKey<Parent>(p => p.UserId);
+        
+        modelBuilder.Entity<Student>()
+            .HasOne(s => s.User)
+            .WithOne()
+            .HasForeignKey<Student>(s => s.UserId);
+        
+        modelBuilder.Entity<Teacher>()
+            .HasOne(t => t.User)
+            .WithOne()
+            .HasForeignKey<Teacher>(t => t.UserId);
+    }
+
+
+}
