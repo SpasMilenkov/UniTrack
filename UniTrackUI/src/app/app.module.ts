fcfe47a28124000f190aad5dde843fb41a23e3d5
mirametodieva <mirushankarr@gmail.com>
2023-11-26 22:30:09 +0200

    Define the main modules

--- UniTrackUI/src/app/app.module.ts
+++ UniTrackUI/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material.module';
+import { AuthModule } from './auth/auth.module';
+import { ProfileModule } from './profile/profile.module';
+import { StatisticsModule } from './statistics/statistics/statistics.module';
+import { LeaderboardModule } from './leaderboard/leaderboard/leaderboard.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +16,12 @@ import { MaterialModule } from './shared/material.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    AuthModule,
+    ProfileModule,
+    StatisticsModule,
+    LeaderboardModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
