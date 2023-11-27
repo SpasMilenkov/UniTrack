aef8fa87d3ec47cd25d59ce1b84e6c0bd9a14599
Spas Milenkov <spasmilenkov123@gmail.com>
2023-11-27 19:33:54 +0200

    Move the auth service to the services assembly

---
+++ UniTrackBackend/UniTrackBackend.Services/Auth/IAuthService.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Identity;
+using UniTrackBackend.Api.ViewModels;
+using UniTrackBackend.Data.Models;
+
+namespace UniTrackBackend.Interfaces;
+
+public interface IAuthService
+{
+    public string GenerateJwtToken(User user);
+    public Task<string> GenerateRefreshToken(User user);
+    public Task<User?> GetUserFromRefreshToken(string refreshToken);
+    public Task<string?> GetEmailConfirmationToken(User user);
+    public Task<User> RegisterUser(RegisterViewModel model);
+    public Task<User> LoginUser(LoginViewModel model);
+    public Task SignInUser(User user);
+    public Task LogoutUser(User user);
+    public Task<IdentityResult> ConfirmEmail(User user, string token);
+    public Task<IdentityResult> ResetPassword(ResetPasswordModel model);
+    public Task<string> GenerateForgottenPasswordLink(User user);
+    public Task<User> GetUserByEmail(string email);
+    public Task<User> GetUserById(string id);
+}
