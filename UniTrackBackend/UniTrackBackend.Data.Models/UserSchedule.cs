60a8131a1d7e65f9c2411a382b1dfb7ccc04c2f2
ted0zh <tedi200220@gmail.com>
2023-11-29 15:00:39 +0200

    added userSchedule model

---
+++ UniTrackBackend/UniTrackBackend.Data.Models/UserSchedule.cs
@@ -0,0 +1,13 @@
+﻿namespace UniTrackBackend.Data.Models
+{ 
+    public class UserSchedule
+    {
+        public int UserId { get; set; }  // Unique identifier for the user
+        public List<ScheduleEvent> Events { get; set; }  // List of events in the user's schedule
+
+        public UserSchedule()
+        {
+            Events = new List<ScheduleEvent>();
+        }
+    }
+}
