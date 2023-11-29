d988c5e33a19a97ec8aa2b28623f8174a479e4c2
Spas Milenkov <spasmilenkov123@gmail.com>
2023-11-29 22:01:30 +0200

    Add middleware for handling exceptions

---
+++ UniTrackBackend/UniTrackBackend/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,69 @@
+using System.Net;
+using System.Text.Json;
+using UniTrackBackend.Services.Commons.Exceptions;
+
+namespace UniTrackBackend.Middlewares;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var code = HttpStatusCode.InternalServerError; // 500 if unexpected
+        string result;
+
+        switch (exception)
+        {
+            case ArgumentNullException e:
+                code = HttpStatusCode.BadRequest;
+                result = e.Message;
+                break;
+            case UnauthorizedAccessException e:
+                code = HttpStatusCode.Unauthorized;
+                result = e.Message;
+                break;
+            case DataNotFoundException e:
+                code = HttpStatusCode.NotFound;
+                result = e.Message;
+                break;
+            case JwtException e:
+                code = HttpStatusCode.BadRequest;
+                result = e.Message;
+                break;
+            case ValidationFailedException e:
+                code = HttpStatusCode.BadRequest;
+                result = e.Message;
+                break;
+            case OperationFailedException e:
+                code = HttpStatusCode.InternalServerError;
+                result = e.Message;
+                break;
+            default:
+                result = "An unexpected error occurred.";
+                break;
+        }
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)code;
+
+        return context.Response.WriteAsync(JsonSerializer.Serialize(new { error = result }));
+    }
+}
