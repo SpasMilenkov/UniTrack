8f9047f6b34653fcd772a97d4af84106a43694a8
ted0zh <tedi200220@gmail.com>
2023-11-29 14:58:25 +0200

    added notification view model

---
+++ UniTrackBackend/UniTrackBackend.Api.ViewModels/NotificationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniTrackBackend.Api.ViewModels
+{
+    public class NotificationViewModel
+    {
+        public int NotificationId { get; set; }
+        public int UserId { get; set; }
+        public int EventId { get; set; }
+        public string Message { get; set; }
+        public DateTime DateCreated { get; set; }
+        public bool IsRead { get; set; }
+    }
+
+}
