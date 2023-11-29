7859f68b86bbb4be232ce242cd29f91eccf28f20
ted0zh <tedi200220@gmail.com>
2023-11-29 14:58:01 +0200

    event view model

---
+++ UniTrackBackend/UniTrackBackend.Api.ViewModels/EventViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace UniTrackBackend.Api.ViewModels
+{
+    public class EventViewModel
+    {
+        public int EventId { get; set; } 
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;    
+        public DateTime Date { get; set; }
+        public string Location { get; set; } = null!;
+       
+    }
+}
