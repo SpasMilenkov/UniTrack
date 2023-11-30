fa744e2b52df2981addf24775bc36ba3be7eccb2
Spas Milenkov <spasmilenkov123@gmail.com>
2023-11-30 22:28:25 +0200

    Start work on a mapper class

---
+++ UniTrackBackend/UniTrackBackend.Services.Mappings/Mapper.cs
@@ -0,0 +1,16 @@
+using UniTrackBackend.Api.ViewModels;
+using UniTrackBackend.Data.Models;
+
+namespace UniTrackBackend.Services.Mappings;
+
+public class Mapper
+{
+    public Mark MapMark(MarkViewModel model)
+    {
+        var mark = new Mark
+        {
+            
+        };
+        return mark;
+    }
+}
