7729fea3f93f16796c0511ad48e48beb9e31e56c
ted0zh <tedi200220@gmail.com>
2023-11-30 22:36:11 +0200

    added controller

---
+++ UniTrackBackend/UniTrackBackend/Controllers/StudentsController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UniTrackBackend.Data.Models;
+using UniTrackBackend.Services;
+
+namespace UniTrackBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentService _studentService;
+
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddStudent([FromBody] Student student)
+        {
+            var createdStudent = await _studentService.AddStudentAsync(student);
+            return CreatedAtAction(nameof(GetStudent), new { id = createdStudent.Id }, createdStudent);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetStudent(int id)
+        {
+            var student = await _studentService.GetStudentByIdAsync(id);
+            if (student == null)
+                return NotFound();
+
+            return Ok(student);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllStudents()
+        {
+            var students = await _studentService.GetAllStudentsAsync();
+            return Ok(students);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateStudent(int id, [FromBody] Student student)
+        {
+            if (id != student.Id)
+                return BadRequest();
+
+            await _studentService.UpdateStudentAsync(student);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudent(int id)
+        {
+            await _studentService.DeleteStudentAsync(id);
+            return NoContent();
+        }
+    }
+
+}
