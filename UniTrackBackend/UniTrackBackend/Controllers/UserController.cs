6e624a9786b643d112db900511961f53b2f2da10
ted0zh <tedi200220@gmail.com>
2023-11-30 23:07:11 +0200

    added controller

---
+++ UniTrackBackend/UniTrackBackend/Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UniTrackBackend.Data.Models;
+using UniTrackBackend.Services;
+
+namespace UniTrackBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AdminController : ControllerBase
+    {
+        private readonly IAdminService _adminService;
+
+        public AdminController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        [HttpPost("CreateUser")]
+        public async Task<IActionResult> CreateUser([FromBody] User user)
+        {
+            var createdUser = await _adminService.CreateUserAsync(user);
+            return CreatedAtAction(nameof(GetUser), new { id = createdUser.Id }, createdUser);
+        }
+
+        [HttpGet("GetUser/{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _adminService.GetUserByIdAsync(id);
+            if (user == null)
+                return NotFound();
+
+            return Ok(user);
+        }
+
+        [HttpGet("GetAllUsers")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _adminService.GetAllUsersAsync();
+            return Ok(users);
+        }
+
+        [HttpPut("UpdateUser/{id}")]    
+        public async Task<IActionResult> UpdateUser(string id, [FromBody] User user)
+        {
+            if (id != user.Id)
+                return BadRequest();
+
+            await _adminService.UpdateUserAsync(user);
+            return NoContent();
+        }
+
+        [HttpDelete("DeleteUser/{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            await _adminService.DeleteUserAsync(id);
+            return NoContent();
+        }
+
+        // Implement other admin-specific endpoints here
+    }
+
+}
