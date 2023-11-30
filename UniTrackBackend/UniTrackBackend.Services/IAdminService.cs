ea53bddcb8a13d127a9efc25dc899368e66f42e3
ted0zh <tedi200220@gmail.com>
2023-11-30 23:08:15 +0200

    added interface

---
+++ UniTrackBackend/UniTrackBackend.Services/IAdminService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniTrackBackend.Data.Models;
+
+namespace UniTrackBackend.Services
+{
+    public interface IAdminService
+    {
+        Task<User> CreateUserAsync(User user);
+        Task<User> GetUserByIdAsync(int id);
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(int id);
+        //implement other admin-specific methods
+    }
+
+}
