2a3606f60c317e773c6bbb7f2b4010747926094d
Spas Milenkov <spasmilenkov123@gmail.com>
2023-12-01 00:10:12 +0200

    Add absences value property and foreign keys

--- UniTrackBackend/UniTrackBackend.Data.Models/Absence.cs
+++ UniTrackBackend/UniTrackBackend.Data.Models/Absence.cs
@@ -3,7 +3,10 @@ namespace UniTrackBackend.Data.Models;
 public class Absence
 {
     public int Id { get; set; }
+    public decimal Value { get; set; }
+    public int TeacherId { get; set; }
     public Teacher Teacher { get; set; } = null!;
+    public int StudentId { get; set; }
     public Student Student { get; set; } = null!;
     public DateTime Time { get; set; }
 }
