02c83da8848b93cdde919951e696dd3a5f745970
ted0zh <tedi200220@gmail.com>
2023-12-01 21:00:01 +0200

    Added absenceViewModel

---
+++ UniTrackBackend/UniTrackBackend.Api.ViewModels/AbsenceViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniTrackBackend.Api.ViewModels
+{
+    public class AbsenceViewModel
+    {
+        public int StudentId { get; set; }
+        public int TeacherId { get; set; }
+        public decimal Value { get; set; } // Assuming Value represents some form of absence value or duration
+        public DateTime Time { get; set; }
+    }
+
+   
+}
