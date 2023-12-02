8f900896612eb0563f45fa5adcc3ab91ebda4fb2
ted0zh <tedi200220@gmail.com>
2023-12-02 12:55:12 +0200

    Added mapTeacher

--- UniTrackBackend/UniTrackBackend.Services.Mappings/IMapper.cs
+++ UniTrackBackend/UniTrackBackend.Services.Mappings/IMapper.cs
@@ -7,4 +7,5 @@ public interface IMapper
 {
     public Mark? MapMark(MarkViewModel model);
     public Absence? MapAbsence(AbsenceViewModel model);
+    public Teacher? MapTeacher(TeacherViewModel model);
 }
