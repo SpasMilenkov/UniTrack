3469b1de0b592ba82e7c4f1126395afbb1ffd977
ted0zh <tedi200220@gmail.com>
2023-12-02 12:41:46 +0200

    Reworked  service

--- UniTrackBackend/UniTrackBackend.Services/TeacherService.cs
+++ UniTrackBackend/UniTrackBackend.Services/TeacherService.cs
@@ -1,55 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UniTrackBackend.Data.Models;
+﻿using UniTrackBackend.Data.Models;
 using UniTrackBackend.Data;
+using Microsoft.Extensions.Logging;
+
 
 namespace UniTrackBackend.Services
 {
     public class TeacherService : ITeacherService
     {
-        private readonly UnitOfWork _unitOfWork;
+        private readonly UnitOfWork _context;
+        private readonly ILogger<TeacherService> _logger;
 
-        public TeacherService(UnitOfWork unitOfWork)
+        public TeacherService(UnitOfWork unitOfWork,ILogger<TeacherService> logger)
         {
-            _unitOfWork = unitOfWork;
+            _context = unitOfWork;
+            _logger = logger;
         }
 
-        public async Task<IEnumerable<Teacher>> GetAllTeachersAsync()
+        public async Task<IEnumerable<Teacher?>?> GetAllTeachersAsync()
         {
-            return await _unitOfWork.TeacherRepository.GetAllAsync();
-        }
-
-        public async Task<Teacher> GetTeacherByIdAsync(int id)
-        {
-
-            return await _unitOfWork.TeacherRepository.GetByIdAsync(id);
-        }
-
-        public async Task<Teacher> AddTeacherAsync(Teacher teacher)
-        {
-            await _unitOfWork.TeacherRepository.AddAsync(teacher);
-            await _unitOfWork.SaveAsync();
-            return teacher;
-        }
-
-        public async Task<Teacher> UpdateTeacherAsync(Teacher teacher)
-        {
-            await _unitOfWork.TeacherRepository.UpdateAsync(teacher);
-            await _unitOfWork.SaveAsync();
-            return teacher;
-        }
-
-        public async Task DeleteTeacherAsync(int id)
-        {
-            var teacher = await _unitOfWork.TeacherRepository.GetByIdAsync(id);
-            if (teacher != null)
+            try
             {
-                await _unitOfWork.TeacherRepository.DeleteAsync(teacher.Id);
-                await _unitOfWork.SaveAsync();
+                return await _context.TeacherRepository.GetAllAsync();
             }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error while trying to get all teachers");
+                return null;
+            }
+
+        }
+
+      
+        public async Task<Teacher?> GetTeacherByIdAsync(int id)
+        {
+            try
+            {
+                return await _context.TeacherRepository.GetByIdAsync(id);
+
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error while getting teacher by ID");
+                return null;
+            }
+
+
+        }
+
+       
+        public async Task<Teacher?> AddTeacherAsync(Teacher? teacher)
+        {
+            try
+            {
+                await _context.TeacherRepository.AddAsync(teacher);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error while adding teacher");
+                return null;
+            }
+            return teacher;
+        }
+
+       
+        public async Task<Teacher?> UpdateTeacherAsync(Teacher? teacher)
+        {
+            try
+            {
+                await _context.TeacherRepository.UpdateAsync(teacher);
+                return teacher;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error while trying to update teacher");
+                return null;
+            }
+        }
+       
+        public async Task<bool> DeleteTeacherAsync(int id)
+        {
+            try
+            {
+                var teacher = await _context.TeacherRepository.GetByIdAsync(id);
+                if (teacher == null) return false;
+
+                await _context.TeacherRepository.DeleteAsync(id);
+                return true;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error while trying to delete teacher");
+                return false;
+            }
+
         }
     }
 
