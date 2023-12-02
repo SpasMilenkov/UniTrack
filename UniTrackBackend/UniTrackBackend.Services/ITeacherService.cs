58044a6a2be63d6d76b892742f0727e37cf03155
ted0zh <tedi200220@gmail.com>
2023-12-02 12:41:30 +0200

    Reworked interface

--- UniTrackBackend/UniTrackBackend.Services/ITeacherService.cs
+++ UniTrackBackend/UniTrackBackend.Services/ITeacherService.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UniTrackBackend.Data.Models;
+﻿using UniTrackBackend.Data.Models;
 
 namespace UniTrackBackend.Services
 {
     public interface ITeacherService
     {
-        Task<IEnumerable<Teacher>> GetAllTeachersAsync();
-        Task<Teacher> GetTeacherByIdAsync(int id);
-        Task<Teacher> AddTeacherAsync(Teacher teacher);
-        Task<Teacher> UpdateTeacherAsync(Teacher teacher);
-        Task DeleteTeacherAsync(int id);
+        Task<IEnumerable<Teacher?>?> GetAllTeachersAsync();
+        Task<Teacher?> GetTeacherByIdAsync(int id);
+        Task<Teacher?> AddTeacherAsync(Teacher? teacher);
+        Task<Teacher?> UpdateTeacherAsync(Teacher? teacher);
+        Task<bool> DeleteTeacherAsync(int id);
     }
 }
