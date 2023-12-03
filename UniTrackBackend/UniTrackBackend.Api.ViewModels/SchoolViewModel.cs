1446934e6eeeb73415d80f7fd642d1c566ae424f
ted0zh <tedi200220@gmail.com>
2023-12-03 19:13:08 +0200

    possible fix

--- UniTrackBackend/UniTrackBackend.Api.ViewModels/SchoolViewModel.cs
+++ UniTrackBackend/UniTrackBackend.Api.ViewModels/SchoolViewModel.cs
@@ -5,9 +5,9 @@ namespace UniTrackBackend.Api.ViewModels
     public class SchoolViewModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
 
-        public List<Teacher> Teachers { get; set; }
-        public List<Student> Students { get; set; }
+        public ICollection<Teacher> Teachers { get; set; } = null!;
+        public ICollection<Student> Students { get; set; } = null!;
     }
 }
