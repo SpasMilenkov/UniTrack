3c1bfeab846bca40cb82d2aae85487d60403f3a5
mirametodieva <mirushankarr@gmail.com>
2023-12-03 18:38:48 +0200

    Update teacher profile interface

--- UniTrackUI/src/app/shared/models/teacher-profile.ts
+++ UniTrackUI/src/app/shared/models/teacher-profile.ts
@@ -2,7 +2,7 @@ import { Profile } from "./profile";
 import { StudentsClass } from "./students-class";
 
 export interface TeacherProfile extends Profile{
-  classes: StudentsClass[]
   id: string;
   subjects: string[];
+  classes: StudentsClass[];
 }
