bcf68cbbbb2901da7ec4d05e99ec78ebfa6c5e14
ted0zh <tedi200220@gmail.com>
2023-12-03 15:35:34 +0200

    Added entity school

---
+++ UniTrackBackend/UniTrackBackend.Data.Models/School.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniTrackBackend.Data.Models
+{
+    public class School
+    {
+        public string Name { get; set; } = null!;
+        public int Id { get; set; }
+        public ICollection Teachers { get; set; } = null!;
+        public ICollection Students { get; set; } = null!;
+    }
+}
