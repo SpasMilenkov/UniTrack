5f56d12deeb1b7227272b944fef4da732835083f
mirametodieva <mirushankarr@gmail.com>
2023-12-03 19:10:08 +0200

    Add add-grade-absence-dialog component

--- UniTrackUI/src/app/shared/components/add-grade-absence-dialog/add-grade-absence-dialog.component.ts
+++ UniTrackUI/src/app/shared/components/add-grade-absence-dialog/add-grade-absence-dialog.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { TeacherDetailsCardTypes } from '../../enums/teacher-details-card-types.enum';
+import { TeacherProfile } from '../../models/teacher-profile';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { UserService } from '../../services/user.service';
+import { AddGradeAbsenceDialogData } from '../../models/add-grade-absence-dialog-data';
 
 @Component({
   selector: 'app-add-grade-absence-dialog',
@@ -6,5 +12,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-grade-absence-dialog.component.scss']
 })
 export class AddGradeAbsenceDialogComponent {
+  types = TeacherDetailsCardTypes;
+  teacher!: TeacherProfile;
+  form = this.fb.group({
+    subject: this.fb.control('', Validators.required),
+    grade: this.fb.control(2, Validators.pattern(/^[2-6]$/)),
+    date: this.fb.control(''),
+  });
 
+  constructor(
+    public dialogRef: MatDialogRef<AddGradeAbsenceDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: AddGradeAbsenceDialogData,
+    private userService: UserService,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.teacher = this.userService.getTeacherProfile();
+  }
+
+  getTypeTitle(): string {
+    let typeTitle = '';
+    switch (this.data.type) {
+      case this.types.ADD_GRADE:
+        typeTitle = 'grade';
+        break;
+      case this.types.ADD_ABSENCE:
+        typeTitle = 'absence';
+        break;
+      case this.types.ADD_EVENT:
+        typeTitle = 'event';
+        break;
+    }
+
+    return typeTitle;
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value);
+    if(this.form.valid){
+      this.dialogRef.close();
+    }
+  }
 }
