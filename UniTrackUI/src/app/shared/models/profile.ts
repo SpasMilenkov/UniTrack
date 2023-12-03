684f9d9e0cc9f086c4625b1a03aceb6d2a6cefaf
mirametodieva <mirushankarr@gmail.com>
2023-12-03 18:06:14 +0200

    Update the profile interface

--- UniTrackUI/src/app/shared/models/profile.ts
+++ UniTrackUI/src/app/shared/models/profile.ts
@@ -2,10 +2,11 @@ import { ProfileTypes } from "../enums/profile-types.enum";
 
 export interface Profile{
   id: string;
+  uniId: string;
   avatarUrl: string;
   firstName: string;
   lastName: string;
   type: ProfileTypes;
-  classNumber: string;
+  classId: string;
   className: string;
 }
