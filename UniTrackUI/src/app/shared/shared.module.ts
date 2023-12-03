c46f98d230123e681992d2a12079ef2930df0031
mirametodieva <mirushankarr@gmail.com>
2023-12-03 18:28:53 +0200

    Update shared module

--- UniTrackUI/src/app/shared/shared.module.ts
+++ UniTrackUI/src/app/shared/shared.module.ts
@@ -5,23 +5,29 @@ import { MaterialModule } from './material.module';
 import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { EventCardComponent } from './components/event-card/event-card.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AddGradeAbsenceDialogComponent } from './components/add-grade-absence-dialog/add-grade-absence-dialog.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
-    EventCardComponent
+    EventCardComponent,
+    AddGradeAbsenceDialogComponent
   ],
   exports: [
     HeaderComponent,
     FooterComponent,
-    EventCardComponent
+    EventCardComponent,
+    AddGradeAbsenceDialogComponent
   ],
   imports: [
     CommonModule,
     MaterialModule,
-    RouterModule
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
