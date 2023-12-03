c809197316c9150ef245f64f83bbcc8838d43dbe
Spas Milenkov <spasmilenkov123@gmail.com>
2023-12-03 15:47:51 +0200

    Add student result and request view models

---
+++ UniTrackBackend/UniTrackBackend.Api.ViewModels/ResultViewModels/StudentResultViewModel.cs
@@ -0,0 +1,56 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace UniTrackBackend.Api.ViewModels.ResultViewModels;
+
+public class StudentResultViewModel
+{
+
+    [Required] public string Id { get; set; } = null!;
+
+    [Required]
+    public string UniId { get; set; } = null!;
+
+    [Required]
+    public string ClassTeacherId { get; set; } = null!;
+
+    [Required]
+    [MaxLength(50)]
+    public string ClassTeacherFirstName { get; set; } = null!;
+
+    [Required]
+    [MaxLength(50)]
+    public string ClassTeacherLastName { get; set; } = null!;
+
+    [Url]
+    public string AvatarUrl { get; set; } = null!;
+
+    [Required]
+    [MaxLength(50)]
+    public string FirstName { get; set; } = null!;
+
+    [Required]
+    [MaxLength(50)]
+    public string LastName { get; set; } = null!;
+
+    [Required]
+    [RegularExpression("STUDENT")]
+    public string Type { get; set; } = null!;
+
+    [Required]
+    public string ClassId { get; set; } = null!;
+
+    [Required]
+    [MaxLength(100)]
+    public string ClassName { get; set; } = null!;
+
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int Number { get; set; }
+
+    public List<MarkViewModel> Marks { get; set; } = null!;
+    [Required]
+    [EmailAddress]
+    public string? Email { get; set; } = null!;
+
+    public List<AbsenceViewModel> Absences { get; set; } = null!;
+}
--- UniTrackBackend/UniTrackBackend.Api.ViewModels/StudentViewModel.cs
+++ UniTrackBackend/UniTrackBackend.Api.ViewModels/StudentViewModel.cs
@@ -4,51 +4,21 @@ namespace UniTrackBackend.Api.ViewModels;
 
 public class StudentViewModel
 {
+    public int StudentId { get; set; }
 
     [Required]
-    public string Id { get; set; }
+    [StringLength(100)]
+    public required string Name { get; set; }
+    
 
     [Required]
-    public string UniId { get; set; }
+    [StringLength(10)]
+    public required string Grade { get; set; }
 
     [Required]
-    public string ClassTeacherId { get; set; }
+    [EmailAddress]
+    [StringLength(100)]
+    public required string Email { get; set; }
+    
 
-    [Required]
-    [MaxLength(50)]
-    public string ClassTeacherFirstName { get; set; }
-
-    [Required]
-    [MaxLength(50)]
-    public string ClassTeacherLastName { get; set; }
-
-    [Url]
-    public string AvatarUrl { get; set; }
-
-    [Required]
-    [MaxLength(50)]
-    public string FirstName { get; set; }
-
-    [Required]
-    [MaxLength(50)]
-    public string LastName { get; set; }
-
-    [Required]
-    [RegularExpression("STUDENT")]
-    public string Type { get; set; }
-
-    [Required]
-    public string ClassId { get; set; }
-
-    [Required]
-    [MaxLength(100)]
-    public string ClassName { get; set; }
-
-    [Required]
-    [Range(1, int.MaxValue)]
-    public int Number { get; set; }
-
-    public List<MarkViewModel> Marks { get; set; }
-
-    // public List<AbsenceViewModel> Absences { get; set; }
 }
