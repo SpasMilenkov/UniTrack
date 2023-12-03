20a19a5f39b1d94e8643dd468a597d7d745ab5ec
mirametodieva <mirushankarr@gmail.com>
2023-12-03 18:07:15 +0200

    Modify the user service

--- UniTrackUI/src/app/shared/services/user.service.ts
+++ UniTrackUI/src/app/shared/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Profile } from '../models/profile';
 import { StudentProfile } from '../models/student-profile';
 import { ProfileTypes } from '../enums/profile-types.enum';
+import { TeacherProfile } from '../models/teacher-profile';
 
 @Injectable({
   providedIn: 'root',
@@ -11,36 +12,84 @@ export class UserService {
 
   getCurrentUserProfile(): Profile | StudentProfile {
     return {
-      id: '1111',
+      id: '2222',
+      uniId: '1',
       type: ProfileTypes.STUDENT,
       avatarUrl: 'assets/images/programmer.png',
       firstName: 'John',
       lastName: 'Doe',
       number: 12345,
-      classNumber: '101',
+      classId: '101',
       className: 'Class A',
       grades: [
-        { subject: 'Math', grade: 6 },
-        { subject: 'Science', grade: 6 },
-        { subject: 'History', grade: 5 },
+        {
+          subject: 'Math',
+          grade: 6,
+          date: '2023-12-01T12:00:00',
+          teacherId: '11111',
+          teacherFirstName: 'John',
+          teacherLastName: 'Doe',
+        },
+        {
+          subject: 'Science',
+          grade: 6,
+          date: '2023-11-21T12:00:00',
+          teacherId: '11111',
+          teacherFirstName: 'John',
+          teacherLastName: 'Doe',
+        },
+        {
+          subject: 'History',
+          grade: 5,
+          date: '2023-12-01T08:00:00',
+          teacherId: '11111',
+          teacherFirstName: 'John',
+          teacherLastName: 'Doe',
+        },
       ],
       absences: [
-        { subject: 'Math', absence: 1, excused: true },
-        { subject: 'Math', absence: 1, excused: true },
-        { subject: 'Science', absence: 1, excused: false },
+        {
+          subject: 'Math',
+          absence: 1,
+          excused: true,
+          date: '2023-12-02T13:00:00',
+          teacherId: '11111',
+          teacherFirstName: 'John',
+          teacherLastName: 'Doe',
+        },
+        {
+          subject: 'Math',
+          absence: 1,
+          excused: true,
+          date: '2023-12-01T13:00:00',
+          teacherId: '11111',
+          teacherFirstName: 'John',
+          teacherLastName: 'Doe',
+        },
+        {
+          subject: 'Science',
+          absence: 1,
+          excused: false,
+          date: '2023-12-01T12:00:00',
+          teacherId: '11111',
+          teacherFirstName: 'John',
+          teacherLastName: 'Doe',
+        },
       ],
     };
   }
 
-  getTeacherProfile(): Profile | StudentProfile {
+  getTeacherProfile(): TeacherProfile {
     return {
       type: ProfileTypes.TEACHER,
+      uniId: '1',
       avatarUrl: 'assets/images/teacher.png',
       firstName: 'John',
       lastName: 'Doe',
       id: '11111',
-      classNumber: '101',
-      className: 'Class A'
-    };
+      classId: '101',
+      className: 'Class A',
+      subjects: ['Math', 'History', 'Music', 'Science'],
+    } as TeacherProfile;
   }
 }
