c4721533646a5ee5986da90edc24531e99e442a9
Spas Milenkov <spasmilenkov123@gmail.com>
2023-12-06 12:05:32 +0200

    Separate schoool service in a directory

---
+++ UniTrackBackend/UniTrackBackend.Services/SchoolService/ISchoolService.cs
@@ -0,0 +1,12 @@
+﻿using UniTrackBackend.Data.Models;
+
+namespace UniTrackBackend.Services;
+
+public interface ISchoolService
+{
+    Task<School?> AddSchoolAsync(School? school);
+    Task<School?> GetSchoolByIdAsync(int id);
+    Task<IEnumerable<School?>?> GetAllSchoolsAsync();
+    Task<School?> UpdateSchoolAsync(School? school);
+    Task<bool> DeleteSchoolAsync(int id);
+}
