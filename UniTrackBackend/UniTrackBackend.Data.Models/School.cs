be3a92cef29c44e100122108e62e92f03c1ebccd
Spas Milenkov <spasmilenkov123@gmail.com>
2023-12-11 23:33:17 +0200

    Fix school relations

--- UniTrackBackend/UniTrackBackend.Data.Models/School.cs
+++ UniTrackBackend/UniTrackBackend.Data.Models/School.cs
@@ -12,7 +12,5 @@ namespace UniTrackBackend.Data.Models
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
-        public ICollection<Teacher> Teachers { get; set; } = null!;
-        public ICollection<Student> Students { get; set; } = null!;
     }
 }
--- UniTrackBackend/UniTrackBackend.Data.Models/Student.cs
+++ UniTrackBackend/UniTrackBackend.Data.Models/Student.cs
@@ -8,6 +8,8 @@ public class Student
     public User User { get; set; } = null!;
     public int GradeId { get; set; }
     public Grade Grade { get; set; } = null!;
+    public int SchoolId { get; set; }
+    public School School { get; set; } = null!;
     public ICollection<Absence> Absences { get; set; } = null!;
     public ICollection<Mark> Marks { get; set; } = null!;
 }
