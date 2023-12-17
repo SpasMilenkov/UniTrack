be7967ef2e53138cda10ae20d3c7e4e7399d6b9a
Spas Milenkov <spasmilenkov123@gmail.com>
2023-12-17 23:41:49 +0200

    Add method for fully loading student based on his user id

--- UniTrackBackend/UniTrackBackend.Data/Repositories/StudentRepository.cs
+++ UniTrackBackend/UniTrackBackend.Data/Repositories/StudentRepository.cs
@@ -41,4 +41,31 @@ public class StudentRepository: EfRepository<Student>, IStudentRepository
         }
         return student;
     }
+    public async Task<Student?> GetStudentWithDetailsAsync(string id)
+    {
+        // Eagerly load directly related entities (e.g., User and Grade)
+        var student = await _context.Students
+            .Include(s => s.User)
+            .Include(s => s.Grade)
+            .ThenInclude(g => g.ClassTeacher)
+            .ThenInclude(t => t.User)
+            .FirstOrDefaultAsync(s => s.UserId == id);
+            
+
+        if (student == null) return student;
+        {
+            // Load Absences - Consider filtering or limiting the results
+            // For example, loading only the recent absences
+            await _context.Entry(student).Collection(s => s.Absences)
+                .Query()
+                .Include(a => a.Subject) // Eagerly load Subject
+                .LoadAsync();
+
+            // Load Marks - Similar considerations as Absences
+            await _context.Entry(student).Collection(s => s.Marks)
+                .Query() // Possibly add filters or sorting
+                .LoadAsync();
+        }
+        return student;
+    }
 }
