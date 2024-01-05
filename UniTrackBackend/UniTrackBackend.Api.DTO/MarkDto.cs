54f247ea80733acf322ddbb17679357c97fd003b
Spas Milenkov <spasmilenkov123@gmail.com>
2024-01-05 22:12:44 +0200

    Add topic to the mark dto

--- UniTrackBackend/UniTrackBackend.Api.DTO/MarkDto.cs
+++ UniTrackBackend/UniTrackBackend.Api.DTO/MarkDto.cs
@@ -2,10 +2,10 @@ namespace UniTrackBackend.Api.DTO;
 
 public class MarkDto
 {
-    public int Id { get; set; }
     public decimal Value { get; set; }
     public int StudentId { get; set; }
     public int TeacherId { get; set; }
     public int SubjectId { get; set; }
+    public required string Topic { get; set; }
     public DateTime GradedOn { get; set; }
 }
