841c9d8372047a6e83658f172be18807412d3a46
Spas Milenkov <spasmilenkov123@gmail.com>
2024-01-05 22:20:27 +0200

    Refactor the teacher service
    Add function to fetch class teacher data

--- UniTrackBackend/UniTrackBackend.Services/TeacherService/ITeacherService.cs
+++ UniTrackBackend/UniTrackBackend.Services/TeacherService/ITeacherService.cs
@@ -1,14 +1,17 @@
-﻿using UniTrackBackend.Data.Models;
+﻿using UniTrackBackend.Api.DTO;
+using UniTrackBackend.Data.Models;
 
 namespace UniTrackBackend.Services;
 
 public interface ITeacherService
 {
     Task<IEnumerable<Teacher>> GetAllTeachersAsync();
-    Task<Teacher> GetTeacherByIdAsync(int id);
+    Task<Teacher> GetTeacherByIdAsync(int id);  
     Task<Teacher?> GetTeacherByUserIdAsync(string userId, bool additionalData);
-    Task<Teacher> AddTeacherAsync(Teacher teacher);
-    Task<Teacher> UpdateTeacherAsync(Teacher teacher);
+    Task<Teacher> AddTeacherAsync(Teacher teacher); 
+    Task<Teacher> UpdateTeacherAsync(TeacherDto teacher, int id);
     Task DeleteTeacherAsync(int id);
+    Task<GradeDto?> GetGradeByClassTeacherId(int id);
     
+
 }
