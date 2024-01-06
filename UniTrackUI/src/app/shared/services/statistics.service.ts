8554b0760a67c626c801a4f4fb3e884e320da146
mirametodieva <mirushankarr@gmail.com>
2024-01-06 17:46:07 +0200

    Add statistic requests

--- UniTrackUI/src/app/shared/services/statistics.service.ts
+++ UniTrackUI/src/app/shared/services/statistics.service.ts
@@ -11,8 +11,13 @@ export class StatisticsService {
 
   constructor(private http: HttpClient, private userService: UserService){}
 
+  getCurrentStudentRecommendationMaterial(): Observable<Statistic> {
+    const {id} = this.userService.getCurrentUserProfile();
+    return this.http.get<Statistic>('http://localhost:5036/Recommendation/ByStudentId/' + id, {withCredentials: true})
+  }
+
   getCurrentStudentStatistics(): Observable<Statistic> {
     const {id} = this.userService.getCurrentUserProfile();
-    return this.http.get<Statistic>('http://localhost:5036/api/Recommendation/' + id, {withCredentials: true})
+    return this.http.get<Statistic>('http://localhost:5036/Analysis/ByStudentId/' + id, {withCredentials: true})
   }
 }
